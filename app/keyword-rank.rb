6e16b44f460c01299f12c083170c9f357edd633c
Jayden Coventry <69566911+jbcoventry@users.noreply.github.com>
2025-07-16 14:48:21 +1000

    works synchronously

--- app/keyword-rank.rb
+++ app/keyword-rank.rb
@@ -2,11 +2,11 @@ require "dotenv/load"
 require "open-uri"
 require "uri"
 require "csv"
+require "json"
 
 CSV.foreach("keywords-short.csv") { |keyword_params|
-  query = keyword_params[0]
-  location = keyword_params[1]
-  response = URI.open("https://serpapi.com/search.json?engine=google&q=#{URI.encode_www_form_component query}&location=#{URI.encode_www_form_component location}&async=true&num=100&api_key=#{ENV["API_KEY"]}").read
-
-  puts response
+  query = URI.encode_www_form(q: keyword_params[0], location: keyword_params[1], async: "true",
+    api_key: ENV["API_KEY"])
+  response = JSON.parse(URI.open("https://httpbin.org/anything?#{query}").read)
+  puts response["args"]
 }
